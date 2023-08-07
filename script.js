b0bec68932a41c3551ffd71209c4c432269bddae
jcha25 <justicech25@gmail.com>
2023-08-07 13:32:43 -0500

    set up current day forecast

--- script.js
+++ script.js
@@ -5,6 +5,12 @@ var current = document.querySelector(".current")
 var forecast = document.querySelector(".forecast")
 var input = document.querySelector(".input")
 
+var info = document.querySelector(".info")
+var icon = document.querySelector(".icon")
+var temp = document.querySelector(".temp")
+var wind = document.querySelector(".wind")
+var hum = document.querySelector(".hum")
+
 searchBtn.addEventListener("click", function(event) {
     event.preventDefault()
     var city = input.value
@@ -12,12 +18,20 @@ searchBtn.addEventListener("click", function(event) {
 })
 
 function getData(cityName) {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${key}`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${key}&units=imperial`)
     .then(function(res) {
         return res.json()
     })
     .then(function(data) {
-        console.log(data)
+        var date = new Date()
+        var day = date.getDate()
+        var month = date.getMonth() + 1
+        var year = date.getFullYear()
+        info.innerHTML = data.name + " " + month + "/" + day + "/" + year
+        icon.setAttribute("src", "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png")
+        icon.setAttribute("alt", data.weather[0].description)
+        temp.innerHTML = "Temp: " + data.main.temp + "&#176F"
+        wind.innerHTML = "Wind: " + data.wind.speed + "mph"
+        hum.innerHTML = "Hum: " + data.main.humidity + "%"
     })
-
 }
